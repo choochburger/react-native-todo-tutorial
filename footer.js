7ed0ee138fb54805f3f0ff058b604986974a3f77
Chris Lyons <chris.lyons@nutonian.com>
2017-01-22 18:38:21 +0100

    add filtering of items

--- footer.js
+++ footer.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default class Footer extends Component {
+class Footer extends Component {
   render() {
+    const { filter } = this.props;
     return (
-      <View></View>
+      <View style={styles.container}>
+        <View style={styles.filters}>
+          <TouchableOpacity
+            style={[styles.filter, filter === 'ALL' && styles.selected]}
+            onPress={() => this.props.onFilter('ALL')}
+          >
+            <Text>All</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.filter, filter === 'ACTIVE' && styles.selected]}
+            onPress={() => this.props.onFilter('ACTIVE')}
+          >
+            <Text>Active</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.filter, filter === 'COMPLETED' && styles.selected]}
+            onPress={() => this.props.onFilter('COMPLETED')}
+          >
+            <Text>Completed</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
+
+Footer.propTypes = {
+  filter: React.PropTypes.oneOf(['ALL', 'ACTIVE', 'COMPLETED']).isRequired,
+  onFilter: React.PropTypes.func.isRequired
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  filters: {
+    flexDirection: 'row'
+  },
+  filter: {
+    padding: 8,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'transparent'
+  },
+  selected: {
+    borderColor: 'rgba(175, 47, 47, 0.2)'
+  }
+});
+
+export default Footer;
