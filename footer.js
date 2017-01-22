632e049004ba08f3304c201a9529e09b34ca4458
Chris Lyons <chris.lyons@nutonian.com>
2017-01-22 16:11:15 +0100

    add input field

--- footer.js
+++ footer.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default class Footer extends Component {
-    render() {
-        return (
-            <View></View>
-        );
-    }
+  render() {
+    return (
+      <View></View>
+    );
+  }
 }
