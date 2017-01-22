32dcf9289c9b07ca7018d7fd544a3144ca956e75
Chris Lyons <chris.lyons@nutonian.com>
2017-01-22 17:12:17 +0100

    toggle rows

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import { View, Text, StyleSheet, Platform, ListView, Keyboard } from 'react-native';
 import Header from './header';
 import Footer from './footer';
+import Row from './row';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
       allComplete: false,
       value: '',
-      items: []
+      items: [],
+      dataSource: ds.cloneWithRows([])
     };
-    this.handleAddItem = this.handleAddItem.bind(this);
+    this.setSource = this.setSource.bind(this);
+    this.handleToggleComplete = this.handleToggleComplete.bind(this);
     this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
+    this.handleAddItem = this.handleAddItem.bind(this);
+  }
+
+  setSource(items, itemsDataSource, otherState = {}) {
+    this.setState({
+      items, // keep track of items that aren't rendered on the screen
+      dataSource: this.state.dataSource.cloneWithRows(itemsDataSource),
+      ...otherState
+    });
+  }
+
+  handleToggleComplete(key, complete) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key !== key) return item;
+      return {
+        ...item,
+        complete
+      }
+    });
+    this.setSource(newItems, newItems);
   }
 
   handleToggleAllComplete() {
@@ -21,10 +45,7 @@ class App extends Component {
       ...item,
       complete
     }));
-    this.setState({
-      items: newItems,
-      allComplete: complete
-    });
+    this.setSource(newItems, newItems, { allComplete: complete });
   }
 
   handleAddItem() {
@@ -37,10 +58,7 @@ class App extends Component {
         complete: false
       }
     ];
-    this.setState({
-      items: newItems,
-      value: ''
-    });
+    this.setSource(newItems, newItems, { value: '' });
   }
 
   render() {
@@ -53,7 +71,26 @@ class App extends Component {
           onToggleAllComplete={this.handleToggleAllComplete}
         />
         <View style={styles.content}>
-
+          <ListView
+            style={styles.list}
+            enableEmptySections
+            dataSource={this.state.dataSource}
+            onScroll={() => Keyboard.dismiss()}
+            renderRow={
+              ({ key, ...value }) => {
+                return (
+                  <Row
+                    key={key}
+                    onComplete={(complete) => this.handleToggleComplete(key, complete)}
+                    {...value}
+                  />
+                );
+              }
+            }
+            renderSeperator={(sectionId, rowId) => {
+              return <View key={rowId} style={styles.separator} />
+            }}
+          />
         </View>
         <Footer />
       </View>
@@ -71,6 +108,13 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1
+  },
+  list: {
+    backgroundColor: '#FFF'
+  },
+  separator: {
+    borderWidth: 1,
+    borderColor: '#F5F5F5'
   }
 });
 
