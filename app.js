380abcc435b74bfb970d72fe7c7c9174b4525d7f
Chris Lyons <chris.lyons@nutonian.com>
2017-01-22 19:09:27 +0100

    edit todos inline

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ class App extends Component {
       items: [],
       dataSource: ds.cloneWithRows([])
     };
+    this.handleUpdateText = this.handleUpdateText.bind(this);
+    this.handleToggleEditing = this.handleToggleEditing.bind(this);
     this.handleClearComplete = this.handleClearComplete.bind(this);
     this.handleFilter = this.handleFilter.bind(this);
     this.setSource = this.setSource.bind(this);
@@ -42,6 +44,28 @@ class App extends Component {
         this.setState({ loading: false });
       }
     });
+  }
+
+  handleUpdateText(key, text) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key !== key) return item;
+      return {
+        ...item,
+        text
+      };
+    })
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
+  }
+
+  handleToggleEditing(key, editing) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key !== key) return item;
+      return {
+        ...item,
+        editing
+      };
+    })
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
   }
 
   setSource(items, itemsDataSource, otherState = {}) {
@@ -122,6 +146,8 @@ class App extends Component {
                 return (
                   <Row
                     key={key}
+                    onUpdate={(text) => this.handleUpdateText(key, text)}
+                    onToggleEdit={(editing) => this.handleToggleEditing(key, editing)}
                     onRemove={() => this.handleRemoveItem(key)}
                     onComplete={(complete) => this.handleToggleComplete(key, complete)}
                     {...value}
--- row.js
+++ row.js
@@ -1,59 +1,107 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Switch, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Switch, TouchableOpacity, TextInput } from 'react-native';
 
 class Row extends Component {
-    render() {
-        const { complete } = this.props;
-        return (
-            <View style={styles.container}>
-                <Switch
-                    value={complete}
-                    onValueChange={this.props.onComplete}
-                />
-                <View style={styles.textWrap}>
-                    <Text
-                        style={[styles.text, complete && styles.complete]}
-                    >
-                        {this.props.text}
-                    </Text>
-                </View>
-                <TouchableOpacity onPress={this.props.onRemove}>
-                    <Text style={styles.destroy}>X</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
+  render() {
+    const { complete } = this.props;
+
+    const textComponent = (
+      <TouchableOpacity
+        style={styles.textWrap}
+        onLongPress={() => this.props.onToggleEdit(true)}
+      >
+        <Text style={[styles.text, complete && styles.complete]}>
+          {this.props.text}
+        </Text>
+      </TouchableOpacity>
+    );
+
+    const removeButton = (
+      <TouchableOpacity onPress={this.props.onRemove}>
+        <Text style={styles.destroy}>X</Text>
+      </TouchableOpacity>
+    );
+
+    const editingComponent = (
+      <View style={styles.textWrap}>
+        <TextInput
+        onChangeText={this.props.onUpdate}
+        autoFocus
+        value={this.props.text}
+        style={styles.input}
+        multiline
+        />
+      </View>
+    );
+
+    const doneButton = (
+      <TouchableOpacity
+        style={styles.done}
+        onPress={() => this.props.onToggleEdit(false)}
+      >
+        <Text stlye={styles.doneText}>Save</Text>
+      </TouchableOpacity>
+    );
+
+    return (
+      <View style={styles.container}>
+        <Switch
+          value={complete}
+          onValueChange={this.props.onComplete}
+        />
+          {this.props.editing ? editingComponent : textComponent}
+          {this.props.editing ? doneButton : removeButton}
+      </View>
+    );
+  }
 }
 
 Row.propTypes = {
-    complete: React.PropTypes.bool.isRequired,
-    onComplete: React.PropTypes.func.isRequired,
-    onRemove: React.PropTypes.func.isRequired,
-    text: React.PropTypes.string.isRequired
+  complete: React.PropTypes.bool.isRequired,
+  onComplete: React.PropTypes.func.isRequired,
+  onRemove: React.PropTypes.func.isRequired,
+  text: React.PropTypes.string.isRequired
 };
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        justifyContent: 'space-between'
-    },
-    textWrap: {
-        flex: 1,
-        marginHorizontal: 10,
-    },
-    complete: {
-        textDecorationLine: 'line-through'
-    },
-    text: {
-        fontSize: 24,
-        color: '#4D4D4D'
-    },
-    destroy: {
-        fontSize: 20,
-        color: '#CC9A9A'
-    }
+  container: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between'
+  },
+  input: {
+    height: 100,
+    flex: 1,
+    fontSize: 24,
+    padding: 0,
+    color: '#4D4D4D'
+  },
+  textWrap: {
+    flex: 1,
+    marginHorizontal: 10,
+  },
+  done: {
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#7BE290',
+    padding: 7
+  },
+  doneText: {
+    color: '#4D4D4D',
+    fontSize: 20
+  },
+  complete: {
+    textDecorationLine: 'line-through'
+  },
+  text: {
+    fontSize: 24,
+    color: '#4D4D4D'
+  },
+  destroy: {
+    fontSize: 20,
+    color: '#CC9A9A'
+  }
 });
 
 export default Row;
